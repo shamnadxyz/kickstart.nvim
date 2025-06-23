746a8f022c5f6a01a95f8b154082e880e9e95d67
shamnadxyz <shamnadxyz@proton.me>
2025-06-23 02:15:52 +0000

    feat: add catppuccin colorscheme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -75,4 +75,18 @@ return {
       }
     end,
   },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        transparent_background = false, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      }
+      -- setup must be called before loading
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
 }
